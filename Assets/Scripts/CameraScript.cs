54efc91f637f40b850514c963dac87555d7f334c
HybridSquirrel <robert.squirrel@mailfence.com>
2020-10-29 14:48:19 +0100

    added tunnel fade

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        gameObject.transform.position = new Vector3(player.transform.position.x, 0 , -1);
+    }
+}
