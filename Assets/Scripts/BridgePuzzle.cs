97eb27642b14d512390d355b1bbe395dd42727a9
HybridSquirrel <robert.squirrel@mailfence.com>
2020-11-12 09:18:27 +0100

    fixed puzzle

---
+++ Assets/Scripts/BridgePuzzle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgePuzzle : MonoBehaviour
+{
+    public bool isUnlocked = false;
+    public bool onBridge = false;
+
+    private void Update()
+    {
+        if (isUnlocked && onBridge)
+        {
+            
+        }
+        else if(isUnlocked)
+        {
+            
+        }
+
+    }
+    
+    
+    
+}
